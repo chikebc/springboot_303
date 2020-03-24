00c9b33ab52d54310010fe870366d61e5caf44b2
chikebc <chikecb@outlook.com>
2020-03-23 21:58:50 -0400

    Initial commit

---
+++ src/main/java/com/springboot_303/springboot_303/CourseRepository.java
@@ -0,0 +1,6 @@
+package com.springboot_303.springboot_303;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourseRepository extends CrudRepository<Course, Long> {
+}
